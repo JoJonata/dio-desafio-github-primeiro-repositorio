2a29b5cfeaca0d15783d85c32250f096d2d1490d
JoJonata <profissional.ojonathan@gmail.com>
2022-07-25 14:48:44 -0300

    Contador copiado da stebsnuch, adicionei as classes´nos botoes e troquei a cor de fundo.

---
+++ Contador/assets/js/scripts.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	if(Count > 0){
+		
+	}
+	CURRENT_NUMBER.innerHTML = count;
+
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
